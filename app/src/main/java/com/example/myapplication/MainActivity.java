1bae0043341c9cb9a880ed30e2b7cb6339ac3fe5
lauracglez <1234solojuegos@gmail.com>
2023-10-09 17:14:32 +0200

    Mostrar resultado pendiente

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextNumero1;
     private EditText editTextNumero2;
     private Spinner spinnerOperacion;
-    private Button buttonCalcular;
     private TextView textViewResultado;
 
     @Override
@@ -22,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final MyApplication myApp = (MyApplication) getApplication();
+
         editTextNumero1 = findViewById(R.id.editTextNumero1);
         editTextNumero2 = findViewById(R.id.editTextNumero2);
         spinnerOperacion = findViewById(R.id.spinnerOperacion);
-        buttonCalcular = findViewById(R.id.buttonCalcular);
+        Button buttonCalcular = findViewById(R.id.buttonCalcular);
         textViewResultado = findViewById(R.id.textViewResultado);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -36,44 +36,33 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerOperacion.setAdapter(adapter);
 
-        buttonCalcular.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calcularResultado();
+        buttonCalcular.setOnClickListener(view -> {
+            double num1 = Double.parseDouble(editTextNumero1.getText().toString());
+            double num2 = Double.parseDouble(editTextNumero2.getText().toString());
+            String operacion = spinnerOperacion.getSelectedItem().toString();
+
+            switch (operacion) {
+                case "+":
+                    myApp.sumar(num1, num2);
+                    break;
+                case "-":
+                    myApp.restar(num1, num2);
+                    break;
+                case "*":
+                    myApp.multiplicar(num1, num2);
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        myApp.dividir(num1, num2);
+                    } else {
+                        textViewResultado.setText(R.string.no_se_puede_dividir_por_cero);
+                    }
+                    break;
             }
+
+            textViewResultado.setText((int) myApp.getResultado());
+
+
         });
-    }
-
-    private void calcularResultado() {
-        String numero1Str = editTextNumero1.getText().toString();
-        String numero2Str = editTextNumero2.getText().toString();
-
-        if (numero1Str.isEmpty() || numero2Str.isEmpty()) {
-            textViewResultado.setText("Debe ingresar ambos números");
-            return;
-        }
-
-        double numero1 = Double.parseDouble(numero1Str);
-        double numero2 = Double.parseDouble(numero2Str);
-
-        String operacion = spinnerOperacion.getSelectedItem().toString();
-        double resultado = 0;
-
-        if (operacion.equals("+")) {
-            resultado = numero1 + numero2;
-        } else if (operacion.equals("-")) {
-            resultado = numero1 - numero2;
-        } else if (operacion.equals("*")) {
-            resultado = numero1 * numero2;
-        } else if (operacion.equals("/")) {
-            if (numero2 != 0) {
-                resultado = numero1 / numero2;
-            } else {
-                textViewResultado.setText("No se puede dividir por cero");
-                return;
-            }
-        }
-
-        textViewResultado.setText("Resultado: " + resultado);
     }
 }
--- app/src/main/java/com/example/myapplication/MyApplication.java
+++ app/src/main/java/com/example/myapplication/MyApplication.java
@@ -3,5 +3,27 @@ package com.example.myapplication;
 import android.app.Application;
 
 public class MyApplication extends Application {
+    private double resultado = 0;
+
+    public void sumar(double numero1, double numero2) {
+        resultado = numero1 + numero2;
+    }
+
+    public void restar(double numero1, double numero2) {
+        resultado = numero1 - numero2;
+    }
+
+    public void multiplicar(double numero1, double numero2) {
+        resultado = numero1 * numero2;
+    }
+
+    public void dividir (double numero1, double numero2) {
+        resultado = numero1 / numero2;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
 
 }
